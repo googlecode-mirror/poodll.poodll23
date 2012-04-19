d8b26f16d1eaff73b7da8ed8d885c37d19db1814
Justin Hunt <bitwalkerjapan@gmail.com>
2012-04-20 00:20:07 +0900

    Added shell of new repo for whiteboard, mp3 audio and standalone video

---
+++ repository/poodllplus/lib.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * repository_poodllplus class
+ * This is a subclass of repository class
+ *
+ * @package    repository_poodllplus
+ * @category   repository
+ * @copyright  2012 Justin Hunt {@link http://www.poodll.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+ //Get our library for uploading. We are a thin wrapper over this
+require_once($CFG->dirroot . '/repository/upload/lib.php');
+ 
+class repository_poodllplus extends repository_upload {
+
+ 
+
+       /**
+     * Return a upload form
+     * @return array
+     */
+    public function get_listing() {
+        global $CFG;
+        $ret = array();
+        $ret['nologin']  = true;
+        $ret['nosearch'] = true;
+        $ret['norefresh'] = true;
+        $ret['list'] = array();
+        $ret['dynload'] = false;
+        $ret['upload'] = array('label'=>get_string('attachment', 'repository'), 'id'=>'repo-form');
+        return $ret;
+    }
+}
