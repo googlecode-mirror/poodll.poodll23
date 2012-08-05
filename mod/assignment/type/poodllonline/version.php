8be0d5123cbc2f89fb20c69563177e42d09dbf39
Justin Hunt <bitwalkerjapan@gmail.com>
2012-08-05 10:27:29 +0900

    updating vesio files ahead of new release

--- mod/assignment/type/poodllonline/version.php
+++ mod/assignment/type/poodllonline/version.php
@@ -22,9 +22,9 @@
  * @copyright  2012 Justin Hunt
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$plugin->version   = 2012070800;
+$plugin->version   = 2012080500;
 $plugin->requires  = 2012062500;
 $plugin->component = 'assignment_poodllonline'; 
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '2.3.2 (Build 2012070800)';
-$plugin->dependencies = array('filter_poodll' => 2012070800);
+$plugin->release   = '2.3.5 (Build 2012080500)';
+$plugin->dependencies = array('filter_poodll' => 2012080500);
