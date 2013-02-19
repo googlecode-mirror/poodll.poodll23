de0f623ccce0bfad4c4c815990e8b543022b7cb6
Justin Hunt <bitwalkerjapan@gmail.com>
2013-02-19 16:58:38 +0900

    more tweaks re api version changes

--- question/type/poodllrecording/question.php
+++ question/type/poodllrecording/question.php
@@ -60,10 +60,11 @@ class qtype_poodllrecording_question extends question_with_responses {
 		global $CFG;
 			//The API for this changed on this date 20120214 (possibly the previous release)
 			//I don't know how to find out. 2.2 branch I think unaffected. Dev unfriendly versions..2.4.1+ and 2.3.4+
-			if ($CFG->build < 2013021400 || $CFG->version < 2012062500){
-				$expecteddata = array('answer' => question_attempt::PARAM_CLEANHTML_FILES);
-			}else{
+			if (($CFG->version > 2012062504.05 && $CFG->version < 2012120300 )|| 
+				($CFG->version > 2012120301.07)){
 				$expecteddata = array('answer' => question_attempt::PARAM_RAW_FILES);
+			}else{
+				$expecteddata = array('answer' => question_attempt::PARAM_CLEANHTML_FILES);
 			}
 			$expecteddata['answerformat'] = PARAM_FORMAT;
 
